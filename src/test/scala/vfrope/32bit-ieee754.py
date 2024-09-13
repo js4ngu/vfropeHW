06472cff52d3c2e0b1d06685e1ffb27f8aa6333b
jongsangYoo <jamin0513@korea.ac.kr>
2024-09-13 19:34:14 +0900

    hex를 리더블하게 변환하는 코드 작성

--- src/test/scala/vfrope/32bit-ieee754.py
+++ src/test/scala/vfrope/32bit-ieee754.py
@@ -9,6 +9,17 @@ def Uint32toIEEE754(a):
     print("=== UINT32 to IEEE754")
     print(a," = ",res)
 
+def ieee754_single_precision_hex_to_float(hex_string):
+    packed = bytes.fromhex(hex_string)
+    num = struct.unpack('>f', packed)[0]
+    return num
+
+def IEEE754toUint32(hex_string):
+    res = ieee754_single_precision_hex_to_float(hex_string)
+    print("=== IEEE754 to UINT32")
+    print(hex_string, " = ", res)
+
+
 def FP32ADD(a,b):
     FP32_a = float_to_ieee754_single_precision_hex(a)
     FP32_b = float_to_ieee754_single_precision_hex(b)
@@ -25,17 +36,21 @@ def FP32MULT(a,b):
     print(f'{a} * {b} = {a*b}')
     print(f'{FP32_a} * {FP32_b} = {FP32_res}')
 
+
+
 def main():
+    '''
     a = 1.5
     b = 2.25
-    '''
     Uint32toIEEE754(-15)
     FP32ADD(a,b)
     FP32MULT(a,b)
-    '''
     FP32ADD(a,b)
     FP32ADD(-a,b)
     FP32ADD(a,-b)
     FP32ADD(-a,-b)
+    Uint32toIEEE754(-15)
+    '''
+    IEEE754toUint32('c0600000')
 
 main()
